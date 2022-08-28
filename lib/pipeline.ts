e68f61beb0114df7ace0c596aefb75e2bcc65a5e
EC2 Default User <ec2-user@ip-172-31-7-11.us-west-2.compute.internal>
2022-08-28 18:35:22 +0000

    updating wave info

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -27,9 +27,9 @@ export default class PipelineConstruct extends Construct {
       .wave({
         id: "envs",
         stages: [
-          { id: "dev", stackBuilder: blueprint.clone('us-west-2')},
-          { id: "test", stackBuilder: blueprint.clone('us-east-2')},
-          { id: "prod", stackBuilder: blueprint.clone('us-east-1')}
+          { id: "dev1", stackBuilder: blueprint.clone('us-west-2')},
+          { id: "test1", stackBuilder: blueprint.clone('us-east-2')},
+          { id: "prod1", stackBuilder: blueprint.clone('us-east-1')}
         ]
       })
       .build(scope, id+'-stack', props);
